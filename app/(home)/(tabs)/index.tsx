ff2f73144396a029461e661f59c2f55e2356b7cf
AllsVeam <alaansvega@gmail.com>
2024-07-11 20:38:26 -0600

    firebase

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import {
+  ScrollView,
+  RefreshControl,
   SafeAreaView,
   View,
   Image,
@@ -8,55 +10,63 @@ import {
   FlatList,
   Text,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
+
+const { width } = Dimensions.get("window");
+
 import { FontAwesome5 } from "@expo/vector-icons";
 
-const categorias = [
-  { id: "1", nombre: "Hoteles", icono: "hotel" },
-  { id: "2", nombre: "Experiencias", icono: "hiking" },
-  { id: "3", nombre: "Restaurantes", icono: "utensils" },
-  { id: "4", nombre: "Aventuras", icono: "mountain" },
-  { id: "5", nombre: "Eventos", icono: "calendar-alt" },
-];
+// import Logo from '../../assets/img/tienda.jpg';
 
-const anuncios = [
-  {
-    id: "1",
-    titulo: "PyME 1",
-    imagen: require("../../assets/img/logo1.png"),
-    descripcion: "Descripción PyME 1.",
-  },
-  {
-    id: "2",
-    titulo: "PyME 2",
-    imagen: require("../../assets/img/logo1.png"),
-    descripcion: "Descripción PyME 2.",
-  },
-  {
-    id: "3",
-    titulo: "PyME 3",
-    imagen: require("../../assets/img/logo1.png"),
-    descripcion: "Descripción PyME 3.",
-  },
-  {
-    id: "4",
-    titulo: "PyME 4",
-    imagen: require("../../assets/img/logo1.png"),
-    descripcion: "Descripción PyME 4.",
-  },
-  {
-    id: "5",
-    titulo: "PyME 5",
-    imagen: require("../../assets/img/logo1.png"),
-    descripcion: "Descripción PyME 5.",
-  },
-];
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { app } from "../../firebase/config";
+import { ikam } from "../../firebase/config-ikam";
+const db = getFirestore(app);
 
 const App = () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [categorias, setCategorias] = useState([]);
   const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(null);
+  const [pymes, setPymes] = useState([]);
+  const [pymeSeleccionada, setPymeSeleccionada] = useState(null);
+
+  const obtenerCategorias = async () => {
+    const querySnapshot = await getDocs(collection(db, "categorias"));
+    const categoriasArray = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setCategorias(categoriasArray);
+  };
+
+  const obtenerPymes = async () => {
+    const querySnapshot = await getDocs(collection(ikam, "pyme"));
+    const pymesArray = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setPymes(pymesArray);
+  };
+
+  useEffect(() => {
+    obtenerCategorias();
+    obtenerPymes();
+  }, []);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await obtenerCategorias();
+    await obtenerPymes();
+    setRefreshing(false);
+  }, []);
 
   const manejarCategoriaPresionada = (categoriaId) => {
     setCategoriaSeleccionada(categoriaId);
+  };
+
+  const manejarPymePresionada = (pymeId) => {
+    setPymeSeleccionada(pymeId);
   };
 
   const renderizarItemCategoria = ({ item }) => (
@@ -76,7 +86,7 @@ const App = () => {
         style={[
           estilos.textoCategoria,
           categoriaSeleccionada === item.id &&
-            estilos.textoCategoriaSeleccionada,
+          estilos.textoCategoriaSeleccionada,
         ]}
       >
         {item.nombre}
@@ -84,12 +94,15 @@ const App = () => {
     </TouchableOpacity>
   );
 
-  const renderizarItemAnuncio = ({ item }) => (
-    <TouchableOpacity style={estilos.tarjeta}>
-      <Image source={item.imagen} style={estilos.imagenTarjeta} />
+  const renderizarItemPyme = ({ item }) => (
+    <TouchableOpacity
+      style={estilos.tarjeta}
+      onPress={() => manejarPymePresionada(item.id)}
+    >
+      <Image source={{ uri: item.imagen1 }} style={estilos.imagenTarjeta} />
       <View style={estilos.detalleTarjeta}>
-        <Text style={estilos.tituloTarjeta}>{item.titulo}</Text>
-        <Text style={estilos.subtituloTarjeta}>{item.descripcion}</Text>
+        <Text style={estilos.tituloTarjeta}>{item.nombre_pyme}</Text>
+        <Text style={estilos.subtituloTarjeta}>{item.direccion}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -102,37 +115,43 @@ const App = () => {
           style={estilos.logo}
         />
       </View>
-      <View style={estilos.barraBusqueda}>
-        <FontAwesome5
-          name="search"
-          size={18}
-          color="#888"
-          style={estilos.iconoBusqueda}
-        />
-        <TextInput
-          style={estilos.entradaBusqueda}
-          placeholder="¿Qué lugar es de tu interés?"
-          placeholderTextColor="#888"
-        />
-      </View>
-      <View style={estilos.contenedorCategorias}>
-        <FlatList
-          data={categorias}
-          renderItem={renderizarItemCategoria}
-          keyExtractor={(item) => item.id}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={estilos.listaCategorias}
-        />
-      </View>
-      <View style={estilos.contenedorAnuncios}>
-        <FlatList
-          data={anuncios}
-          renderItem={renderizarItemAnuncio}
-          keyExtractor={(item) => item.id}
-          contentContainerStyle={estilos.listaAnuncios}
-        />
-      </View>
+        <View style={estilos.barraBusqueda}>
+          <FontAwesome5
+            name="search"
+            size={18}
+            color="#888"
+            style={estilos.iconoBusqueda}
+          />
+          <TextInput
+            style={estilos.entradaBusqueda}
+            placeholder="¿Qué lugar es de tu interés?"
+            placeholderTextColor="#888"
+          />
+        </View>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        <View style={estilos.contenedorCategorias}>
+          <FlatList
+            data={categorias}
+            renderItem={renderizarItemCategoria}
+            keyExtractor={(item) => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={estilos.listaCategorias}
+          />
+        </View>
+        <View style={estilos.contenedorPymes}>
+          <FlatList
+            data={pymes}
+            renderItem={renderizarItemPyme}
+            keyExtractor={(item) => item.id}
+            contentContainerStyle={estilos.listaPymes}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -159,7 +178,7 @@ const estilos = StyleSheet.create({
     alignItems: "center",
     marginTop: 20,
     marginBottom: 10,
-    width: "90%",
+    width: width * 0.9,
     height: 50,
     backgroundColor: "#FFF",
     borderRadius: 20,
@@ -213,39 +232,44 @@ const estilos = StyleSheet.create({
   textoCategoriaSeleccionada: {
     color: "#000",
   },
-  contenedorAnuncios: {
+  contenedorPymes: {
     flex: 1,
     paddingHorizontal: 20,
     paddingTop: 10,
   },
-  listaAnuncios: {
+  listaPymes: {
     paddingBottom: 20,
   },
   tarjeta: {
-    flexDirection: "row",
-    backgroundColor: "#FFF",
-    borderRadius: 10,
-    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    overflow: "hidden",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    elevation: 3,
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    width: width * .90,
+    marginBottom: 20,
+    alignSelf: "center",
   },
   imagenTarjeta: {
-    width: 120,
-    height: 120,
+    width: 500,
+    height: 250,
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
-    resizeMode: "cover",
+    resizeMode: "stretch",
   },
   detalleTarjeta: {
     flex: 1,
-    padding: 10,
+    padding: 20,
   },
   tituloTarjeta: {
     fontSize: 18,
     fontWeight: "bold",
+    textAlign: "center"
   },
   subtituloTarjeta: {
     fontSize: 14,
